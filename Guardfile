f89e62c370cfda5af4668f2a64abcc0a85baa996
hirosuke999 <hirosuke999@hoge.com>
2015-02-11 14:04:06 +0900

    piyo test

--- Guardfile
+++ Guardfile
@@ -2,7 +2,7 @@
 # More info at https://github.com/guard/guard#readme
 require 'active_support/inflector'
 
-guard 'rspec', after_all_pass: true, cli: '--drb' do
+guard 'rspec', after_all_pass: false, cli: '--drb' do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe User, :focus => true do
+describe User do
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
       password: "foobar", password_confirmation: "foobar")
@@ -35,7 +35,7 @@ describe User, :focus => true do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-              foo@bar_baz.com foo@bar+baz.com]
+              foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -61,6 +61,16 @@ describe User, :focus => true do
     end
     
     it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
   
   describe "when password is not present" do
